ea36eb7697497b53bd288a19edae727715f3ec9f
Felipe Soares <felipeluizsoares@gmail.com>
2017-01-01 20:23:20 -0200

    Add name attribute to HelloWorld component

--- app/index.js
+++ app/index.js
@@ -3,13 +3,15 @@ var ReactDOM = require('react-dom')
 
 var HelloWorld = React.createClass({
   render: function () {
+    console.log(this.props)
+    
     return (
-      <div> Hello World </div>
+      <div> Hello {this.props.name} </div>
     )
   }
 })
 
 ReactDOM.render(
-  <HelloWorld />,
+  <HelloWorld name="Felipe"/>,
   document.getElementById('app')
 )
