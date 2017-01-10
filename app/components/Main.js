56cab81ad898ad985e78df565ff56e677754ec41
Felipe Soares <felipeluizsoares@gmail.com>
2017-01-10 11:57:22 -0200

    Add React container and presentational components

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,10 +3,9 @@ var React = require('react');
 var Main = React.createClass({
   render: function () {
     return (
-      <div className='main-container'> Hello from main!
+      <div className='main-container'>
         {this.props.children}
       </div>
-
     )
   }
 });
