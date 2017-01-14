d134ef94d89935f15022e59845ec1811fa2a3b18
Felipe Soares <felipeluizsoares@gmail.com>
2017-01-14 16:16:35 -0200

    Add Battle UI

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -2,34 +2,42 @@ var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
 var githubHelpers = require('../utils/githubHelpers');
 
-var ConfirmBattleReact = React.createClass({
+var ConfirmBattleContainer = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
-
   getInitialState: function () {
     return {
       isLoading: true,
-      playerInfo: []
+      playersInfo: [],
     }
   },
-
   componentDidMount: function () {
     var query = this.props.location.query;
     githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
       .then(function (players) {
         this.setState({
           isLoading: false,
-          playerInfo: [players[0], players[1]]
+          playersInfo: [players[0], players[1]]
         })
       }.bind(this))
   },
-
+  handleInitiateBattle: function () {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
   render: function () {
     return (
-      <ConfirmBattle isLoading={this.state.isLoading} playerInfo={this.state.playerInfo}/>
-    );
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
+        playersInfo={this.state.playersInfo} />
+    )
   }
 });
 
-module.exports = ConfirmBattleReact;
+module.exports = ConfirmBattleContainer;
